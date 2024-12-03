b22ee9c0216f1983cf4a071a076eb0166aa9185a
Bigodeee46 <guilhermebrandao1224@gmail.com>
2024-12-03 11:32:43 -0300

    AULA 2 SOBRE VARIAVEIS

---
+++ Aula 02.py
@@ -0,0 +1,9 @@
+print('Salve Meus Parceiros')
+nome_usuario = 'Bigode' # tipo -> str = string
+idade_usuario = 17 # fan 160 vermelha
+altura_usuario =  1.70 # tipo -> floot = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+print('Seja Bem-vindo', nome_usuario,idade_usuario,altura_usuario,maior_idade)
+print('sua idade é', idade_usuario,'anos')
+print('vc tem', altura_usuario, "metros.")
+print('voce é maior de idade?', maior_idade)
